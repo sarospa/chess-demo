9fbc5f1392451d7d5f6fe3f1bd02b71c250d6d32
Christopher Rohrbaugh <ffivfan@hotmail.com>
2014-03-23 15:18:35 -0700

    First commit.

---
+++ Assets/Script/BoardCont.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardCont : MonoBehaviour
+{
+	public GameObject whiteSquare;
+	public GameObject blackSquare;
+	public GameObject whitePawn;
+	public GameObject blackPawn;
+	public const float corner = -0.42f;
+	public const float squareSize = 0.12f;
+	public const float pieceScale = 0.9f;
+	public const int boardSize = 8;
+
+	public static bool whiteTurn = true;
+	public static GameObject[,] pieces;
+	public static GameObject selectedPiece = null;
+	public static float xScale;
+	public static float yScale;
+
+	void Start ()
+	{
+		pieces = new GameObject[boardSize,boardSize];
+		for (int x = 0; x < boardSize; x++)
+		{
+			for (int y = 0; y < boardSize; y++)
+			{
+				if ((x + y) % 2 == 1)
+				{
+					CreateSquare (whiteSquare, x, y);
+				}
+				else
+				{
+					CreateSquare (blackSquare, x, y);
+				}
+			}
+		}
+		for (int x = 0; x < boardSize; x++)
+		{
+			CreatePiece (whitePawn, x, 1);
+			CreatePiece (blackPawn, x, 6);
+		}
+	}
+	
+	void Update () {
+		xScale = (float)Screen.width / Mathf.Max (new int[2] {Screen.width, Screen.height}) * Camera.main.orthographicSize * 2;
+		yScale = (float)Screen.height / Mathf.Max (new int[2] {Screen.width, Screen.height}) * Camera.main.orthographicSize * 2;
+		if (selectedPiece != null)
+		{
+			selectedPiece.transform.position = new Vector3
+			(
+				(Input.mousePosition.x - Screen.width / 2) / Screen.width * xScale,
+				(Input.mousePosition.y - Screen.height / 2) / Screen.height * yScale,
+				-3
+			);
+		}
+	}
+
+	static Vector3 CellLoc (int x, int y, float z)
+	{
+		return new Vector3 (corner + (squareSize*x), corner + (squareSize*y), z);
+	}
+
+	void CreateSquare (GameObject square, int x, int y)
+	{
+		GameObject clone = (GameObject)Instantiate (square);
+		clone.transform.parent = this.transform;
+		clone.transform.localPosition = CellLoc (x, y, -1f);
+		clone.transform.localScale = new Vector3 (squareSize, squareSize, 1);
+	}
+
+	void CreatePiece (GameObject piece, int x, int y)
+	{
+		GameObject clone = (GameObject)Instantiate (piece);
+		clone.transform.parent = this.transform;
+		clone.transform.localPosition = CellLoc (x, y, -2f);
+		clone.transform.localScale = new Vector3 (squareSize * pieceScale, squareSize * pieceScale, 1);
+		PieceCont controller = clone.GetComponent<PieceCont> ();
+		controller.SetLoc (x, y);
+		pieces [x, y] = clone;
+	}
+
+	public static void PlacePiece (GameObject piece, int x, int y)
+	{
+		piece.transform.localPosition = CellLoc (x, y, -2f);
+		pieces [x, y] = piece;
+		pieces [x, y].GetComponent<PieceCont> ().SetLoc (x, y);
+	}
+}
---
+++ Assets/Script/PieceCont.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PieceCont : MonoBehaviour {
+	private bool selected;
+	private Vector3 loc;
+	private int xLoc;
+	private int yLoc;
+	public string pieceType;
+	public string color;
+	private bool moved = false;
+
+	void OnMouseDown ()
+	{
+		if ((color == "white") == BoardCont.whiteTurn)
+		{
+			xLoc = (int)((this.gameObject.transform.localPosition.x + BoardCont.squareSize * BoardCont.boardSize / 2) / BoardCont.squareSize);
+			yLoc = (int)((this.gameObject.transform.localPosition.y + BoardCont.squareSize * BoardCont.boardSize / 2) / BoardCont.squareSize);
+			loc = this.gameObject.transform.localPosition;
+			BoardCont.selectedPiece = this.gameObject;
+			selected = true;
+		}
+	}
+
+	void OnMouseUp ()
+	{
+		if (selected)
+		{
+			int x = Mathf.FloorToInt ((this.gameObject.transform.localPosition.x + BoardCont.squareSize * 4) / BoardCont.squareSize);
+			int y = Mathf.FloorToInt ((this.gameObject.transform.localPosition.y + BoardCont.squareSize * 4) / BoardCont.squareSize);
+			List<Vector2> moves = CheckValidMoves ();
+			if (moves.Contains (new Vector2 (x, y)))
+			{
+				BoardCont.pieces[x, y] = this.gameObject;
+				BoardCont.pieces[xLoc, yLoc] = null;
+				BoardCont.PlacePiece (this.gameObject, x, y);
+				moved = true;
+				BoardCont.whiteTurn = (color != "white");
+			}
+			else
+			{
+				this.gameObject.transform.localPosition = loc;
+			}
+			BoardCont.selectedPiece = null;
+			selected = false;
+		}
+	}
+
+	public void SetLoc (int x, int y)
+	{
+		if (x >= 0 && x < BoardCont.boardSize && y >= 0 && y < BoardCont.boardSize)
+		{
+			xLoc = x;
+			yLoc = y;
+		}
+	}
+
+	string CheckSpace (int x, int y)
+	{
+		if (x < 0 || x >= BoardCont.boardSize || y < 0 || y >= BoardCont.boardSize)
+		{
+			return "invalid";
+		}
+		if (BoardCont.pieces[x, y] == null)
+		{
+			return "empty";
+		}
+		if (BoardCont.pieces[x, y].GetComponent<PieceCont>().color == this.color)
+		{
+			return "ally";
+		}
+		else
+		{
+			return "enemy";
+		}
+	}
+
+	public List<Vector2> CheckValidMoves ()
+	{
+		List<Vector2> moves = new List<Vector2> ();
+		switch (pieceType)
+		{
+		case "pawn":
+			int dir;
+			if (color == "white")
+			{
+				dir = 1;
+			}
+			else
+			{
+				dir = -1;
+			}
+			if (CheckSpace (xLoc, yLoc+dir) == "empty")
+			{
+				moves.Add (new Vector2 (xLoc, yLoc+dir));
+				if (!moved && CheckSpace (xLoc, yLoc+dir*2) == "empty")
+				{
+					moves.Add (new Vector2 (xLoc, yLoc+dir*2));
+				}
+			}
+			break;
+		}
+		return moves;
+	}
+}
